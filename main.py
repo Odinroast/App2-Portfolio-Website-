3af63f0e047fc8372a0473c8a9756ba062cbbaa8
Ruthvick <ruthvick2005@gmail.com>
2023-01-05 22:55:44 -0500

    Rendered the application Titles

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import streamlit
+import pandas as pd
 import streamlit as st
+
 st.set_page_config(layout="wide")
 col1, col2 = st.columns(2)
 
@@ -16,5 +17,20 @@ with col2:
 contact_content = """
 Below you can find some of the apps that I have built in python, Feel free to contact me!
 """
-streamlit.write(contact_content)
+st.write(contact_content)
+
+data = pd.read_csv("data.csv", sep=';')
+col3, col4 = st.columns(2)
+with col3:
+    for index, row in data[:11].iterrows():
+        st.header(row["title"])
+
+with col4:
+    for index, row in data[10:].iterrows():
+        st.header(row["title"])
+        print(index)
+
+
+
+
 
